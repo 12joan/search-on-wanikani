c1099c6f2e3db48cd3461865e02306e3da6346be
Joe Anderson <joe@mousetrapped.co.uk>
2023-01-12 17:28:48 +0000

    Restore fork
    This fork was originally created by Royalnb, with whose permission I am
    now re-publishing it on GitHub.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Search-on-Kitsun.io
-Select any text on the webpage and right-click to open the context menu where you can select "Search on Kitsun.io". This will take you straight to Kitsun.io and look up the highlighted text in the dictionary.
+# Search on WaniKani
+Select any text on the webpage and right-click to open the context menu where you can select "Search Kanji on Wanikani" and "Search Vocabulary on Wanikani". This will take you straight to wanikani.com and look up the highlighted Kanji or Vocabulary.
--- background.js
+++ background.js
@@ -1,13 +1,29 @@
 browser.contextMenus.create({
-    id: "KitsunSearch",
-    title: "Search on Kitsun.io",
-    contexts: ["selection"]
+    id: "WanikaniKanjiSearch",
+    title: "Search Kanji on Wanikani",
+    contexts: ["selection"],
+    "icons": {
+        "16": "icons/kanji16.png",
+        "32": "icons/kanji32.png"
+      }
 });
 
-browser.contextMenus.onClicked.addListener(contextMenuAction);
+browser.contextMenus.create({
+    id: "WanikaniVocabularySearch",
+    title: "Search Vocabulary on Wanikani",
+    contexts: ["selection"],
+    "icons": {
+        "16": "icons/vocab16.png",
+        "32": "icons/vocab32.png"
+      }
+});
 
-function contextMenuAction(info, tab){
-    const url = "https://kitsun.io/dictionary/"+ info.selectionText;
-
-    browser.tabs.create({url:url});
-}
+browser.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "WanikaniKanjiSearch") {
+        const url = "https://www.wanikani.com/kanji/"+ info.selectionText;
+        browser.tabs.create({url:url});
+    } else if (info.menuItemId === "WanikaniVocabularySearch") {
+        const url = "https://www.wanikani.com/vocabulary/"+ info.selectionText;
+        browser.tabs.create({url:url});
+    }
+})
