4998e5ef23b5c27548f1fb527467add31c4d03f6
Joe Anderson <joe@mousetrapped.co.uk>
2023-01-12 18:03:39 +0000

    Trim and URL-encode selected text

--- background.js
+++ background.js
@@ -1,29 +1,32 @@
 browser.contextMenus.create({
-    id: "WaniKaniKanjiSearch",
-    title: "Search Kanji on WaniKani",
-    contexts: ["selection"],
-    "icons": {
-        "16": "icons/kanji16.png",
-        "32": "icons/kanji32.png"
-      }
+  id: "WaniKaniKanjiSearch",
+  title: "Search Kanji on WaniKani",
+  contexts: ["selection"],
+  "icons": {
+    "16": "icons/kanji16.png",
+    "32": "icons/kanji32.png"
+  }
 });
 
 browser.contextMenus.create({
-    id: "WaniKaniVocabularySearch",
-    title: "Search Vocabulary on WaniKani",
-    contexts: ["selection"],
-    "icons": {
-        "16": "icons/vocab16.png",
-        "32": "icons/vocab32.png"
-      }
+  id: "WaniKaniVocabularySearch",
+  title: "Search Vocabulary on WaniKani",
+  contexts: ["selection"],
+  "icons": {
+    "16": "icons/vocab16.png",
+    "32": "icons/vocab32.png"
+  }
 });
 
 browser.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "WaniKaniKanjiSearch") {
-        const url = "https://www.wanikani.com/kanji/"+ info.selectionText;
-        browser.tabs.create({url:url});
-    } else if (info.menuItemId === "WaniKaniVocabularySearch") {
-        const url = "https://www.wanikani.com/vocabulary/"+ info.selectionText;
-        browser.tabs.create({url:url});
-    }
-})
+  const type = {
+    "WaniKaniKanjiSearch": "kanji",
+    "WaniKaniVocabularySearch": "vocabulary"
+  }[info.menuItemId];
+
+  const query = encodeURIComponent(info.selectionText.trim());
+
+  const url = `https://www.wanikani.com/${type}/${query}`;
+
+  browser.tabs.create({ url });
+});
